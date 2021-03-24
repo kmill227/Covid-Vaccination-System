4afb5fceadb2ede63ae51d2aa1a551c886d8f8a0
Robert Silvey <rsilvey2@kent.edu>
2021-03-23 22:10:06 -0400

    Update Vaccine.h

---
+++ Vaccine.h
@@ -0,0 +1,18 @@
+#ifndef VACCINE_H
+#define VACCINE_H
+
+#include <stdlib>
+
+class Vaccine {
+
+	public:
+
+	private:
+		std::string Brand;
+		double price;
+		int ID
+		int date
+		int dose_number
+}
+
+#endif
