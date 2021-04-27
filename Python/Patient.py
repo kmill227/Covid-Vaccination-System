cff9347a8b9fa70358fe3216c36c8528fcf13c76
kmill227 <kmill227@kent.edu>
2021-04-27 11:42:44 -0400

    Update Patient.py

--- Python/Patient.py
+++ Python/Patient.py
@@ -1,12 +1,5 @@
-from Vaccine import Vaccine
-from Insurance import Insurance 
-class Patient:
 
-    def __init__(self):
-        self.email = " "
-        self.password = " "
-        self.vaccine = Vaccine()
-        self.insurance = Insurance()
-        self.appointments = {}
+class Patient:
+    pass
     
     
