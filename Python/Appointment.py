a77b38a99ddf3c8f127ca57f7a81e15d5b80b8ca
kmill227 <kmill227@kent.edu>
2021-04-27 20:55:58 -0400

    change appointment

--- Python/Appointment.py
+++ Python/Appointment.py
@@ -4,9 +4,7 @@ from Employee import Employee
 from Campus import Campus
 class Appointment:
     def __init__(self):
-        self.receiver = Patient()
-        self.giver = Employee()
-        self.date = datetime.date()
-        self.time = datetime.time()
+        self.patient = Patient()
+        self.date = datetime.datetime()
         self.campus = Campus()
         
