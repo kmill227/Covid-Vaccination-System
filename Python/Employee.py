82b183973face63bab86fc5cdd81567f18c21b70
kmill227 <kmill227@kent.edu>
2021-04-27 20:13:49 -0400

    changes

--- Python/Employee.py
+++
@@ -1,5 +0,0 @@
-from User import User
-class Employee(User): 
-    
-    def __init__(self):
-        super.__init__()
--- Python/User.py
+++ Python/User.py
@@ -3,6 +3,7 @@ class User:
     def __init__(self): 
         self.email = " "
         self.password = " "
+        self.id = 0
     
     def logIn(self):
         pass
