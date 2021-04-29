c3e282b62969af97ec35e756b4f52d62221eb778
Robert Silvey <rsilvey2@kent.edu>
2021-04-29 12:38:53 -0400

    started on cancelApp

--- Python/Patient.py
+++ Python/Patient.py
@@ -2,6 +2,7 @@ from User import User
 from Insurance import Insurance
 from Appointment import Appointment
 from Vaccine import Vaccine
+from DataBaseConnection import DataBase
 class Patient(User):
     
     def __init__(self):
@@ -9,6 +10,7 @@ class Patient(User):
         self.ins = Insurance()
         self.vaccine = Vaccine()
         self.appointments = []
+        self.name = ""
 
     def createAppointment(self):
         pass
@@ -17,7 +19,12 @@ class Patient(User):
         pass
 
     def cancelAppointment(self):
-        pass
+        cancel = DataBase()
+        cancel.connection.cursor.execute(
+            "SELECT * FROM user WHERE ID = appoint user id"
+            
+            
+            )
 
     def rescheduleAppointment(self):
         pass
