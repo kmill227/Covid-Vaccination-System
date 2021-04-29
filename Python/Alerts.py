3756d869db5435c0574669be6037dfb6f9ff1ea0
kmill227 <kmill227@kent.edu>
2021-04-29 14:48:05 -0400

    update alerts.py

--- Python/Alerts.py
+++ Python/Alerts.py
@@ -1,9 +1,20 @@
+from DataBaseConnection import DataBase
 class Alerts: 
     def __init__(self):
-        pass
+        self.send = False
+
+    def lowVaccines(self, Campus):
+        db = DataBase()
+        db.cursor.execute("SELECT VaccinesOnHand FROM campus WHERE CampusName = '%s'", Campus)
+        VaccineCount = db.cursor.fetchall()
+        print(VaccineCount)
 
     def sendEmail(self):
         pass
 
     def displayOnMenu(self):
         pass
+
+
+a = Alerts()
+a.lowVaccines("Stark")
