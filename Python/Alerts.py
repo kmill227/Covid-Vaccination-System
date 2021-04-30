c0195a83d758776338361e1835a922313ce320b0
kmill227 <kmill227@kent.edu>
2021-04-30 18:19:10 -0400

    check low vaccines method

--- Python/Alerts.py
+++ Python/Alerts.py
@@ -2,16 +2,6 @@ from DataBaseConnection import DataBase
 class Alerts: 
     def __init__(self):
         self.send = False
-
-    def lowVaccines(self, Campus):
-        db = DataBase()
-        sql = "SELECT VaccinesOnHand FROM campus WHERE CampusName=%s"
-        arg = (Campus, )
-        db.cursor.execute(sql, arg)
-        vaccineCount = db.cursor.fetchone()
-        if vaccineCount == 0: 
-            self.sendEmail()
-        
         
 
     def sendEmail(self):
--- Python/Campus.py
+++ Python/Campus.py
@@ -1,9 +1,27 @@
+from Alerts import Alerts
+from DataBaseConnection import DataBase
 class Campus:
     def __init__(self):
         self.name = " "
-        self.numberOfVaccines = 0
-        self.isRegional = False 
+        self.vaccineCount = 0
+        self.isRegional = 1 
         self.revenue = 0
+        self.alerts = Alerts()
 
+    def lowVaccines(self):
+        db = DataBase()
+        sql = "SELECT VaccinesOnHand FROM campus WHERE CampusName=%s"
+        arg = (self.name, )
+        db.cursor.execute(sql, arg)
+        self.vaccineCount = db.cursor.fetchone()
+        sql = "SELECT isRegional FROM campus WHERE CampusName=%s"
+        db.cursor.execute(sql, arg)
+        self.isRegional = db.cursor.fetchone()
+        if self.vaccineCount == 0: 
+            alerts.sendEmail()
+        if (self.vaccineCount < 50 and self.isRegional == 1) or (self.vaccineCount < 150 and self.isRegional == 0):
+            self.orderVaccines()
+        
+        
     def orderVaccines(self):
         pass
